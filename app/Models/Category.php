af053aee51fef40035117360c8b8572682625540
Muhammad Yudi Setiawan <yudsetiawann@gmail.com>
2025-12-07 17:50:32 +0700

    update: & -> dan

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use App\Models\Post;
+use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -11,6 +12,20 @@ class Category extends Model
 {
     /** @use HasFactory<\Database\Factories\CategoryFactory> */
     use HasFactory;
+
+    protected $fillable = ['name', 'slug'];
+    protected static function booted()
+    {
+        static::creating(function ($category) {
+            // Cek apakah slug kosong, jika ya, buat dari name
+            if (empty($category->slug)) {
+                // Ubah & jadi dan
+                $text = str_replace('&', 'dan', $category->name);
+                // Buat slug
+                $category->slug = Str::slug($text);
+            }
+        });
+    }
 
     public function posts(): HasMany
     {
